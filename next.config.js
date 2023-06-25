42b3524d2dc91036175c8d30907861addba1d7c8
Ryota Tsunoi <ryota.tsunoi@enlarge-llc.com>
2023-06-25 14:17:19 +0900

    fix: fix output mode

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+// NOTE: For Github page deployment, we need to set the output mode to 'export'
+const outputMode = process.env.BRANCH_NAME ? 'export' : 'standalone';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'standalone',
+  output: outputMode,
 };
 
 module.exports = nextConfig;
